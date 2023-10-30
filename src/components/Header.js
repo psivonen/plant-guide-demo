f72716ba8aa83c4452b645cb08a5e04327fe7853
psivonen <petra.sivonen@gmail.com>
2023-10-30 10:03:05 +0200

    setup gh-pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Menu = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <NavLink className="nav-link" to="/">
+                <NavLink className="nav-link" to="/plant-guide-demo">
                   Search
                 </NavLink>
               </Nav>
